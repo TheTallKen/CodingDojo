6ce3a53bc7958b9427edd30ead0180c9dce1015d
Ken G. Barton <Ken@minnotank.com>
2018-08-14 13:33:05 -0500

    First commit

---
+++ thaifood.js
@@ -0,0 +1,14 @@
+
+
+function tip(bill);{
+    console.log(bill * 1.2);
+    return bill * 1.2;
+}
+
+var thaifood = 150;
+var group = 4; 
+var total = tip(thaifood);
+tip(thaifood);
+
+total/group
+
